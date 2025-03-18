621a0c3e5c8f0706e61e27fd4c54452660cd5e23
igorcandidodev <ygoraveiro@gmail.com>
2025-03-17 22:03:25 -0300

    first commit

---
+++ src/main/java/com/runningapi/runningapi/RunningapiApplication.java
@@ -0,0 +1,13 @@
+package com.runningapi.runningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningapiApplication.class, args);
+	}
+
+}
